24e6cf51e891c63ae1872df3152dadf6bb7a9cd7
parthsunilpatil <parth.patil@imaginea.com>
2019-11-06 20:40:52 +0530

    Hello World Application for Techtonic

---
+++ src/main/java/com/example/demo/HelloWorldApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+@Controller
+public class HelloWorldApplication {
+	
+	@GetMapping("/")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="Techtonic") String name, Model model) {
+		model.addAttribute("name", name);
+        return "greeting";
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+}
